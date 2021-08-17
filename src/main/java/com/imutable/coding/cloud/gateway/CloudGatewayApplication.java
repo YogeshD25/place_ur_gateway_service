d72c8679a4998d651e6a78181abafbc664758534
Yogesh Dande <dandeyogesh25@gmail.com>
2021-08-17 11:15:28 +0530

    gateway added

--- src/main/java/com/imutable/coding/cloud/gateway/CloudGatewayApplication.java
+++ src/main/java/com/imutable/coding/cloud/gateway/CloudGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.imutable.coding.cloud.gateway;
 
 import com.imutable.coding.cloud.gateway.repository.UserRepository;
+import io.r2dbc.spi.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,8 +12,12 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 import org.springframework.data.r2dbc.core.DatabaseClient;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -23,6 +29,7 @@ import java.util.stream.BaseStream;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableR2dbcAuditing
 //@EnableHystrix
 public class CloudGatewayApplication {
 	@Autowired
@@ -32,4 +39,13 @@ public class CloudGatewayApplication {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
 
+	@Bean
+	ConnectionFactoryInitializer initializer(@Qualifier("connectionFactory") ConnectionFactory connectionFactory) {
+		ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+		initializer.setConnectionFactory(connectionFactory);
+		ResourceDatabasePopulator resource =
+				new ResourceDatabasePopulator(new ClassPathResource("schema.sql"));
+		initializer.setDatabasePopulator(resource);
+		return initializer;
+	}
 }
